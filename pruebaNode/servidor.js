77c814656b790ce523a673e0d543e6b6b287d046
Iván R. Galicia <irodgal@gmail.com>
2016-01-27 16:55:49 +0100

    Commit para ver modificaciones

--- pruebaNode/servidor.js
+++ pruebaNode/servidor.js
@@ -2,11 +2,11 @@ var http = require('http');
 var server = http.createServer();
 
 function control(petic, resp) { 
-	console.log('peticion pillada!');
+	console.log('petición pillada!');
 	resp.writeHead(200, {'content-type': 'text/plain'});
 	resp.write('Hola, Mundo!');
 	resp.end();
 }
 
 server.on('request', control);
-server.listen(8080);
+server.listen(18080);
